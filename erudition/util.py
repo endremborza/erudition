ba94b7b2693a750114ea0e5b538e31984be71fbc
Endre Márk Borza <endremborza@gmail.com>
2022-02-16 14:07:41 +0100

    name+email git

--- erudition/util.py
+++ erudition/util.py
@@ -4,7 +4,7 @@ from invoke import UnexpectedExit
 def git_commit(c, addstr, msg):
     try:
         c.run("git config --get user.email")
-        c.run("git config --get user.email")
+        c.run("git config --get user.name")
     except UnexpectedExit:
         c.run('git config --local user.email "ci@cd.org"')
         c.run('git config --local user.name "CI/CD"')
