5570cce3801456f30ebce34ad96224454626a790
Endre Márk Borza <endremborza@gmail.com>
2022-02-16 15:12:58 +0100

    tinker __main__

--- erudition/__main__.py
+++ erudition/__main__.py
@@ -2,10 +2,19 @@ import sys
 
 from .data_challenge.setup_dir import create_dir
 
-if __name__ == "__main__":
+
+def main(args):
     try:
-        arg = sys.argv[1]
+        arg = args[1]
     except IndexError:
         arg = ""
     if arg == "challenge":
-        create_dir(sys.argv[2])
+        try:
+            target = args[3]
+        except IndexError:
+            target = "."
+        create_dir(args[2], target)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main(sys.argv)
