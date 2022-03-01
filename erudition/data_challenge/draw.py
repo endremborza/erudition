35c1bc0a3c87179a56e24b6f7d6c206ddcd574c8
Endre Márk Borza <endremborza@gmail.com>
2022-03-01 20:30:19 +0100

    draw based on dirhash

--- erudition/data_challenge/draw.py
+++ erudition/data_challenge/draw.py
@@ -1,34 +1,65 @@
 import json
 from pathlib import Path
+from subprocess import check_output
 
 import numpy as np
 import pandas as pd
+from checksumdir import dirhash
 
 from . import constants as const
 
 
+def checkout(target):
+    check_output(["git", "checkout", target])
+
+
+def _add_link_col(gdf):
+    return gdf.assign(
+        v=[
+            f"[v{i + 1}](../../commit/{gdf['true_commit'].iloc[i]})"
+            for i in range(gdf.shape[0])
+        ]
+    )
+
+
 def dump_readme():
     lpath = Path(const.LOG_DIR)
+    base_df = pd.DataFrame(
+        map(json.loads, map(Path.read_text, lpath.glob("*.json")))
+    )
+
+    hash_recs = []
+    for _rec in (
+        base_df[["name", "commit", "at"]]
+        .drop_duplicates(subset=["name", "commit"])
+        .to_dict("records")
+    ):
+        checkout(_rec["commit"])
+        _md = dirhash(_rec["name"], "md5")
+        hash_recs.append({"dirhash": _md, **_rec})
+    checkout("main")
+
+    true_c_df = pd.DataFrame(hash_recs).pipe(
+        lambda df: df.merge(
+            df.sort_values("at")
+            .groupby(["name", "dirhash"])["commit"]
+            .first()
+            .rename("true_commit")
+            .reset_index()
+        ).drop("at", axis=1)
+    )
+
     fresh_things = (
-        pd.DataFrame(
-            map(json.loads, map(Path.read_text, lpath.glob("*.json")))
-        )
+        base_df.merge(true_c_df)
         .sort_values("at")
         .pipe(
             lambda _df: _df.merge(
-                _df.groupby(["name", "commit"])["at"]
+                _df.groupby(["name", "true_commit"])["at"]
                 .first()
                 .sort_values()
                 .reset_index()
                 .groupby("name")
-                .apply(
-                    lambda gdf: gdf.assign(
-                        v=[
-                            f"[v{i + 1}](../../commit/{gdf['commit'].iloc[i]})"
-                            for i in range(gdf.shape[0])
-                        ]
-                    )
-                )[["v", "commit", "name"]]
+                .apply(_add_link_col)[["v", "true_commit", "name"]]
             )
         )
         .groupby(["v", "name", "input_id"])
